698858714ec499aadeb9fcbc49fc1e3b7f1777cb
Joshua Patterson <jleegippies@gmail.com>
2019-11-02 15:02:43 -0500

    Added a ball and diagnostic controller for it

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This should be used for diagnostic purposes to move the ball in the scene.
+/// </summary>
+public class BallController : MonoBehaviour
+{
+    public float speed;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 velocity = input.normalized * speed * Time.fixedDeltaTime;
+        rb.AddForce(velocity);
+    }
+}
