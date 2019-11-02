6624b4f687e3eebff90a0bfa80208d0a3f5a7861
Joshua Patterson <jleegippies@gmail.com>
2019-11-02 15:24:38 -0500

    Added camera controls

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float speed;
+    public float rotateSpeed;
+
+    void Update() {
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Jump"), Input.GetAxisRaw("Vertical"));
+        Vector3 velocity = input.normalized * speed * Time.fixedDeltaTime;
+        float rotateAngle = Input.GetAxisRaw("Rotate") * rotateSpeed * Time.fixedDeltaTime;
+
+        transform.Translate(velocity, Space.Self);
+        transform.Rotate(Vector3.up, rotateAngle);
+    }
+}
