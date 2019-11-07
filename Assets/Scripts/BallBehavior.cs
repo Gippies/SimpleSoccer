155507577204b69992ee48fd207f68591d2bfafb
Joshua Patterson <jleegippies@gmail.com>
2019-11-06 21:31:11 -0600

    Added collision with goal and ability to keep score

---
+++ Assets/Scripts/BallBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallBehavior : MonoBehaviour
+{
+    public Vector3 startPosition;
+    public ScoreManager scoreManager;
+
+    Rigidbody thisRigidbody;
+
+    void Start() {
+        thisRigidbody = GetComponent<Rigidbody>();
+    }
+
+    void OnTriggerEnter(Collider other) {
+        if (other.transform.parent.name == "Goal West") {
+            scoreManager.addScore(Goal.West);
+            ResetBall();
+            print("Collided object: " + other.transform.parent.name + " score: " + scoreManager.getScore(Goal.West));
+        }
+    }
+
+    void ResetBall() {
+        transform.position = startPosition;
+        transform.rotation = Quaternion.Euler(Vector3.zero);
+        thisRigidbody.velocity = Vector3.zero;
+        thisRigidbody.angularVelocity = Vector3.zero;
+    }
+}
---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    int westScore;
+    int eastScore;
+    // Start is called before the first frame update
+    void Start()
+    {
+        westScore = 0;
+        eastScore = 0;
+    }
+
+    public void addScore(Goal g) {
+        if (g == Goal.West)
+            westScore++;
+        else if (g == Goal.East)
+            eastScore++;
+    }
+
+    public int getScore(Goal g) {
+        if (g == Goal.West)
+            return westScore;
+        else if (g == Goal.East)
+            return eastScore;
+        else
+            return -1;
+    }
+
+    public void resetScore() {
+        westScore = 0;
+        eastScore = 0;
+    }
+}
+
+public enum Goal {
+    East,
+    West
+}
