75589c11045372d0762a992646b9ecc31d704758
Joshua Patterson <jleegippies@gmail.com>
2019-11-06 20:38:31 -0600

    Fixed camera movement, added crosshair, added ability to kick ball

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -6,27 +6,50 @@ public class CameraController : MonoBehaviour
 {
     public float speed;
     public float mouseSensitivity;
+    public float kickMultiplier;
+    public float kickHeightMultiplier;
 
     float rotateAngleX;
     float rotateAngleY;
+    Camera thisCamera;
 
     void Start() {
         rotateAngleX = 0.0f;
         rotateAngleY = 0.0f;
+
+        thisCamera = GetComponent<Camera>();
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
     void Update() {
+        MoveCamera();
+        KickRigidBody();
+    }
+
+    void MoveCamera() {
         Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Jump"), Input.GetAxisRaw("Vertical"));
         Vector3 velocity = input.normalized * speed * Time.deltaTime;
-        rotateAngleX += Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
-        rotateAngleY -= Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+        rotateAngleX += Input.GetAxis("Mouse X") * mouseSensitivity;
+        rotateAngleY -= Input.GetAxis("Mouse Y") * mouseSensitivity;
 
         rotateAngleY = Mathf.Clamp(rotateAngleY, -90.0f, 90.0f);
 
         transform.Translate(velocity, Space.Self);
         transform.eulerAngles = new Vector3(rotateAngleY, rotateAngleX);
+    }
+
+    void KickRigidBody() {
+        if (Input.GetMouseButtonDown(0)) {
+            Ray cameraRay = thisCamera.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(cameraRay, out hit)) {
+                // the object identified by hit.transform was clicked
+                if (hit.transform.gameObject.name == "Soccer Ball" && Vector3.Distance(transform.position, hit.transform.position) < 3.0f) {
+                    hit.transform.gameObject.GetComponent<Rigidbody>().AddForce(cameraRay.direction * kickMultiplier + Vector3.up * kickHeightMultiplier);
+                }
+            }
+        }
     }
 }
